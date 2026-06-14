0e30d971b51b2e3620358194f8515883692080c6
QuocChienn2709 <hackiosipa@gmail.com>
2026-06-14 15:20:35 +0700

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -9,62 +9,32 @@ router = Router()
 
 @router.message(Command("start"))
 async def start(msg: Message):
-    await users.update_one(
-        {"user_id": msg.from_user.id},
-        {"$setOnInsert": {"username": msg.from_user.username, "created_at": msg.date}},
-        upsert=True
-    )
-    await msg.answer(
-        "👋 <b>Chào mừng đến với cửa hàng Tài Khoản Pro!</b>\n\n"
-        "Chọn chức năng bên dưới:",
-        reply_markup=main_menu(),
-        parse_mode="HTML"
-    )
+    await users.update_one({"user_id": msg.from_user.id},
+                           {"$setOnInsert": {"username": msg.from_user.username}}, upsert=True)
+    await msg.answer("👋 <b>Chào mừng đến Cửa hàng Tài Khoản Pro!</b>", reply_markup=main_menu())
 
 @router.callback_query(F.data == "shop")
 async def shop(callback: CallbackQuery):
-    categories = await products.distinct("category")
-    if not categories:
-        await callback.answer("Chưa có sản phẩm nào. Liên hệ admin!", show_alert=True)
-        return
-    await callback.message.edit_text("📂 <b>Chọn loại sản phẩm:</b>", reply_markup=categories_kb(categories), parse_mode="HTML")
+    cats = await products.distinct("category")
+    await callback.message.edit_text("📂 <b>Chọn loại sản phẩm:</b>", reply_markup=categories_kb(cats))
 
 @router.callback_query(F.data.startswith("cat_"))
 async def show_products(callback: CallbackQuery):
-    category = callback.data.split("_")[1]
-    prods = await products.find({"category": category}).to_list(50)
-    
-    if not prods:
-        await callback.answer("Không có sản phẩm trong danh mục này!")
-        return
-    
-    await callback.message.edit_text(
-        f"📦 <b>Sản phẩm {category.capitalize()}:</b>",
-        reply_markup=products_kb(prods),
-        parse_mode="HTML"
-    )
+    cat = callback.data.split("_")[1]
+    prods = await products.find({"category": cat}).to_list(30)
+    await callback.message.edit_text(f"📦 <b>{cat.capitalize()}</b>", reply_markup=products_kb(prods))
 
 @router.callback_query(F.data.startswith("prod_"))
 async def product_detail(callback: CallbackQuery):
-    try:
-        prod_id = callback.data.split("_")[1]
-        prod = await products.find_one({"_id": ObjectId(prod_id)})
-        
-        if not prod:
-            await callback.answer("Sản phẩm không tồn tại!")
-            return
-            
-        text = f"""
+    prod_id = callback.data.split("_")[1]
+    prod = await products.find_one({"_id": ObjectId(prod_id)})
+    if not prod:
+        return await callback.answer("Sản phẩm không tồn tại!")
+    
+    text = f"""
 🛒 <b>{prod['name']}</b>
-
-💰 Giá: <b>{prod['price']:,} đ</b>
+💰 Giá: <b>{prod['price']:,}đ</b>
 ⏳ Thời hạn: <b>{prod['duration']}</b>
-📦 Tồn kho: <b>{prod.get('stock', 0)}</b> tài khoản
+📦 Tồn kho: <b>{prod.get('stock', 0)}</b>
 """
-        await callback.message.edit_text(text, reply_markup=payment_method_kb(prod_id), parse_mode="HTML")
-    except:
-        await callback.answer("Lỗi xử lý sản phẩm!")
-
-@router.callback_query(F.data == "main")
-async def back_to_main(callback: CallbackQuery):
-    await callback.message.edit_text("Chọn chức năng:", reply_markup=main_menu())
+    await callback.message.edit_text(text, reply_markup=payment_method_kb(str(prod['_id'])))
